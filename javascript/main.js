478747a7f2c3f92f7d440633de5b7b7d6f8ef1f7
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-17 15:20:29 +0100

    inizio lavoro

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+// Partendo dai file html e css che vi allego, implementate la funzionalità slider.
+// Se clicchiamo sulle icone next e prev andiamo avanti o indietro con le visualizzazioni. Il giro deve ripartire se siamo sull'ultima o la prima.
+// I cerchietti devono cambiare colore contemporaneamente.
+// BONUS
+// Aggingiamo la possibilità di andare avanti e indietro con la tastiera premendo le frecce destra/sinistra.
